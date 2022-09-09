a8ac22bd31d5c9ff96d6b8d84ce2a58577e6e104
Ludovic <98219549+Malsius@users.noreply.github.com>
2022-09-09 17:01:07 +0200

    Update update-resources.sh
    - Output more quiet
    - Patch wget links to latest packages from GitHub
    - Patch link to AzureHound to the latest seen version
    - Patch password setting for wso-webshell on mac
    - Patch ysoserial download
    - Edit command to init and update submodules
    - Add nc static binary

--- update-resources.sh
+++ update-resources.sh
@@ -29,7 +29,7 @@ function prepare_filesystem() {
   mkdir -p ./webshells/PHP/
   mkdir -p ./webshells/ASPX/
   mkdir -p ./webshells/JSP/
-  mkdir -p "./encrypted_disks/"
+  mkdir -p ./encrypted_disks/
 }
 
 function chkfs() {
@@ -40,7 +40,7 @@ function chkfs() {
 function add_sysinternals() {
   info "Downloading SysinternalsSuite"
   chkfs "./windows/"
-  wget -nv -O ./windows/SysinternalsSuite.zip "https://download.sysinternals.com/files/SysinternalsSuite.zip"
+  wget -q -O ./windows/SysinternalsSuite.zip "https://download.sysinternals.com/files/SysinternalsSuite.zip"
   unzip -q -o -d ./windows/SysinternalsSuite ./windows/SysinternalsSuite.zip
   rm ./windows/SysinternalsSuite.zip
 }
@@ -48,54 +48,54 @@ function add_sysinternals() {
 function add_pspy() {
   info "Downloading pspy"
   chkfs "./linux/pspy/"
-  wget -nv -O ./linux/pspy/pspy32 "$(curl -s https://github.com/DominicBreuker/pspy/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/pspy32"
-  wget -nv -O ./linux/pspy/pspy64 "$(curl -s https://github.com/DominicBreuker/pspy/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/pspy64"
-  wget -nv -O ./linux/pspy/pspy32s "$(curl -s https://github.com/DominicBreuker/pspy/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/pspy32s"
-  wget -nv -O ./linux/pspy/pspy64s "$(curl -s https://github.com/DominicBreuker/pspy/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/pspy64s"
+  wget -q -O ./linux/pspy/pspy32 "https://github.com/DominicBreuker/pspy/releases/latest/download/pspy32"
+  wget -q -O ./linux/pspy/pspy64 "https://github.com/DominicBreuker/pspy/releases/latest/download/pspy64"
+  wget -q -O ./linux/pspy/pspy32s "https://github.com/DominicBreuker/pspy/releases/latest/download/pspy32s"
+  wget -q -O ./linux/pspy/pspy64s "https://github.com/DominicBreuker/pspy/releases/latest/download/pspy64s"
 }
 
 function add_peass() {
   info "Downloading PEAS Suite"
   chkfs "./linux/linPEAS/"
-  wget -nv -O ./linux/linPEAS/linpeas.sh "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/linpeas.sh"
-  wget -nv -O ./linux/linPEAS/linpeas_darwin_amd64 "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/linpeas_darwin_amd64"
-  wget -nv -O ./linux/linPEAS/linpeas_darwin_arm64 "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/linpeas_darwin_arm64"
-  wget -nv -O ./linux/linPEAS/linpeas_linux_386 "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/linpeas_linux_386"
-  wget -nv -O ./linux/linPEAS/linpeas_linux_amd64 "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/linpeas_linux_amd64"
-  wget -nv -O ./linux/linPEAS/linpeas_linux_arm "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/linpeas_linux_arm"
-  wget -nv -O ./linux/linPEAS/linpeas_linux_arm64 "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/linpeas_linux_arm64"
+  wget -q -O ./linux/linPEAS/linpeas.sh "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh"
+  wget -q -O ./linux/linPEAS/linpeas_darwin_amd64 "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_darwin_amd64"
+  wget -q -O ./linux/linPEAS/linpeas_darwin_arm64 "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_darwin_arm64"
+  wget -q -O ./linux/linPEAS/linpeas_linux_386 "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_386"
+  wget -q -O ./linux/linPEAS/linpeas_linux_amd64 "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64"
+  wget -q -O ./linux/linPEAS/linpeas_linux_arm "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_arm"
+  wget -q -O ./linux/linPEAS/linpeas_linux_arm64 "https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_arm64"
   chkfs "./windows/winPEAS/"
-  wget -nv -O ./windows/winPEAS/winPEAS.bat "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/winPEAS.bat"
-  wget -nv -O ./windows/winPEAS/winPEASany.exe "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/winPEASany.exe"
-  wget -nv -O ./windows/winPEAS/winPEASany_ofs.exe "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/winPEASany_ofs.exe"
-  wget -nv -O ./windows/winPEAS/winPEASx64.exe "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/winPEASx64.exe"
-  wget -nv -O ./windows/winPEAS/winPEASx64_ofs.exe "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/winPEASx64_ofs.exe"
-  wget -nv -O ./windows/winPEAS/winPEASx86.exe "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/winPEASx86.exe"
-  wget -nv -O ./windows/winPEAS/winPEASx86_ofs.exe "$(curl -s https://github.com/carlospolop/PEASS-ng/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/winPEASx86_ofs.exe"
+  wget -q -O ./windows/winPEAS/winPEAS.bat "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS.bat"
+  wget -q -O ./windows/winPEAS/winPEASany.exe "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany.exe"
+  wget -q -O ./windows/winPEAS/winPEASany_ofs.exe "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany_ofs.exe"
+  wget -q -O ./windows/winPEAS/winPEASx64.exe "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64.exe"
+  wget -q -O ./windows/winPEAS/winPEASx64_ofs.exe "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64_ofs.exe"
+  wget -q -O ./windows/winPEAS/winPEASx86.exe "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86.exe"
+  wget -q -O ./windows/winPEAS/winPEASx86_ofs.exe "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86_ofs.exe"
 }
 
 function add_linux_smart_enumeration() {
   info "Downloading Linux Smart Enumeration"
   chkfs "./linux/"
-  wget -nv -O ./linux/linux-smart-enumeration.sh "https://github.com/diego-treitos/linux-smart-enumeration/raw/master/lse.sh"
+  wget -q -O ./linux/linux-smart-enumeration.sh "https://github.com/diego-treitos/linux-smart-enumeration/raw/master/lse.sh"
 }
 
 function add_linenum() {
   info "Downloading LinEnum"
   chkfs "./linux/"
-  wget -nv -O ./linux/LinEnum.sh "https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh"
+  wget -q -O ./linux/LinEnum.sh "https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh"
 }
 
 function add_linux_exploit_suggester() {
   info "Downloading Linux Exploit Suggester"
   chkfs "./linux/"
-  wget -nv -O ./linux/linux-exploit-suggester.sh "https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh"
+  wget -q -O ./linux/linux-exploit-suggester.sh "https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh"
 }
 
 function add_mimikatz() {
   info "Downloading mimikatz"
   chkfs "./windows/"
-  wget -nv -O ./windows/mimikatz_trunk.zip "$(curl -s https://github.com/gentilkiwi/mimikatz/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/mimikatz_trunk.zip"
+  wget -q -O ./windows/mimikatz_trunk.zip "https://github.com/gentilkiwi/mimikatz/releases/latest/download/mimikatz_trunk.zip"
   unzip -q -o -d ./windows/mimikatz/ ./windows/mimikatz_trunk.zip
   rm ./windows/mimikatz_trunk.zip
 }
@@ -103,88 +103,91 @@ function add_mimikatz() {
 function add_sharphound() {
   info "Downloading SharpHound"
   chkfs "./windows/"
-  wget -nv -O ./windows/SharpHound.exe "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.exe"
+  wget -q -O ./windows/SharpHound.exe "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.exe"
 }
 
 function add_juicypotato() {
   info "Downloading JuicyPotato"
   chkfs "./windows/"
-  wget -nv -O ./windows/JuicyPotato.exe "$(curl -s https://github.com/ohpe/juicy-potato/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/JuicyPotato.exe"
+  wget -q -O ./windows/JuicyPotato.exe "https://github.com/ohpe/juicy-potato/releases/latest/download/JuicyPotato.exe"
 }
 
 function add_nc() {
   info "Downloading nc for Windows"
   chkfs "./windows/"
-  wget -nv -O ./windows/nc.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/nc.exe"
-  # chkfs "./linux/"
-  # todo add nc.traditional for linux
+  wget -q -O ./windows/nc.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/nc.exe"
+  info "Downloading nc for Linux"
+  chkfs "./linux/"
+  wget -q -O ./linux/nc "http://www.stearns.org/nc/nc-static"
 }
 
 function add_spoolsample() {
   info "Downloading SpoolSample"
   chkfs "./windows/"
-  wget -nv -O ./windows/SpoolSample.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/SpoolSample.exe"
-  wget -nv -O ./windows/SpoolSample_v4.5_x64.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/SpoolSample_v4.5_x64..exe"
+  wget -q -O ./windows/SpoolSample.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/SpoolSample.exe"
+  wget -q -O ./windows/SpoolSample_v4.5_x64.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/SpoolSample_v4.5_x64..exe"
 }
 
 function add_diaghub() {
   info "Downloading DiagHub"
   chkfs "./windows/"
-  wget -nv -O ./windows/diaghub.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/diaghub.exe"
+  wget -q -O ./windows/diaghub.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/diaghub.exe"
 }
 
 function add_lazagne() {
   info "Downloading LaZagne"
-  git -C ./ clone https://github.com/AlessandroZ/LaZagne
+  git -C ./ clone -q https://github.com/AlessandroZ/LaZagne
   chkfs "./linux/"
+  rm -rf ./linux/LaZagne
   mv ./LaZagne/Linux ./linux/LaZagne
   chkfs "./mac/"
+  rm -rf ./mac/LaZagne
   mv ./LaZagne/Mac ./mac/LaZagne
   chkfs "./windows/"
+  rm -rf ./windows/LaZagne
   mv ./LaZagne/Windows ./windows/LaZagne
-  wget -nv -O ./windows/LaZagne/lazagne.exe "$(curl -s https://github.com/AlessandroZ/LaZagne/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')/lazagne.exe"
+  wget -q -O ./windows/LaZagne/lazagne.exe "https://github.com/AlessandroZ/LaZagne/releases/latest/download/lazagne.exe"
   rm -rf ./LaZagne
 }
 
 function add_sublinacl() {
   info "Downloading Sublinacl"
   chkfs "./windows/"
-  wget -nv -O ./windows/sublinacl.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/sublinacl.exe"
+  wget -q -O ./windows/sublinacl.exe "https://gitlab.com/onemask/pentest-tools/-/raw/master/windows/sublinacl.exe"
 }
 
 function add_plink() {
   info "Downloading plink"
   chkfs "./windows/"
-  wget -nv -O ./windows/plink32.exe "https://the.earth.li/~sgtatham/putty/latest/w32/plink.exe"
-  wget -nv -O ./windows/plink64.exe "https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe"
+  wget -q -O ./windows/plink32.exe "https://the.earth.li/~sgtatham/putty/latest/w32/plink.exe"
+  wget -q -O ./windows/plink64.exe "https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe"
 }
 
 function add_deepce() {
   info "Downloading deepce"
   chkfs "./linux/"
-  wget -nv -O ./linux/deepce.sh "https://github.com/stealthcopter/deepce/raw/master/deepce.sh"
+  wget -q -O ./linux/deepce.sh "https://github.com/stealthcopter/deepce/raw/master/deepce.sh"
 }
 
 function add_webshells() {
   info "Downloading webshells"
   chkfs "./webshells/PHP/wso-webshell/"
-  wget -nv -O ./webshells/PHP/wso-webshell/wso.php "https://raw.githubusercontent.com/mIcHyAmRaNe/wso-webshell/master/wso.php"
-  sed -i 's/fa769dac7a0a94ee47d8ebe021eaba9e/0fc3bcf177377d328c77b2b51b7f3c9b/g' ./webshells/PHP/wso-webshell/wso.php
+  wget -q -O ./webshells/PHP/wso-webshell/wso.php "https://raw.githubusercontent.com/mIcHyAmRaNe/wso-webshell/master/wso.php"
+  perl -i -pe's/fa769dac7a0a94ee47d8ebe021eaba9e/0fc3bcf177377d328c77b2b51b7f3c9b/g' ./webshells/PHP/wso-webshell/wso.php
   echo 'exegol4thewin' > ./webshells/PHP/wso-webshell/password.txt
   # Setting password to exegol4thewin
   chkfs "./webshells/ASPX"
-  wget -nv -O ./webshells/ASPX/webshell.aspx "https://raw.githubusercontent.com/xl7dev/WebShell/master/Aspx/ASPX%20Shell.aspx"
+  wget -q -O ./webshells/ASPX/webshell.aspx "https://raw.githubusercontent.com/xl7dev/WebShell/master/Aspx/ASPX%20Shell.aspx"
 }
 
 function add_ysoserial_net() {
   info "Downloading ysoserial"
   chkfs "./windows/"
-  url=$(curl -s https://github.com/pwntester/ysoserial.net/releases/latest | grep -o '"[^"]*"' | tr -d '"' | sed 's/tag/download/')
-  tag=${url##*/}
-  prefix=${tag:1}
-  wget -nv -O ./ysoserial_net.zip "$url/ysoserial-$prefix.zip"
-  unzip -d ./windows/ ./ysoserial_net.zip
-  rm -r ./windows/ysoserial.net
+  url=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/pwntester/ysoserial.net/releases/latest)
+  version=${url##*v}
+  wget -q -O ./ysoserial_net.zip "https://github.com/pwntester/ysoserial.net/releases/latest/download/ysoserial-$version.zip"
+  unzip -q -d ./windows/ ./ysoserial_net.zip
+  rm -rf ./windows/ysoserial.net
   mv ./windows/Release/ ./windows/ysoserial.net
   rm ./ysoserial_net.zip
 }
@@ -192,19 +195,18 @@ function add_ysoserial_net() {
 function add_http-put-server() {
   info "Downloading http-put-server for Python3"
   chkfs "./linux/"
-  wget -nv -O ./linux/http-put-server.py https://gist.githubusercontent.com/mildred/67d22d7289ae8f16cae7/raw/214c213c9415da18a471d1ed04660022cce059ef/server.py
+  wget -q -O ./linux/http-put-server.py "https://gist.githubusercontent.com/mildred/67d22d7289ae8f16cae7/raw/214c213c9415da18a471d1ed04660022cce059ef/server.py"
 }
 
 function add_azurehound() {
   info "Downloading AzureHound"
   chkfs "./windows/"
-  wget -nv -O ./windows/AzureHound.ps1 "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/AzureHound.ps1"
+  wget -q -O ./windows/AzureHound.ps1 "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/45c04e7292a44362af1031cc0d7bd3748433ac4d/Collectors/AzureHound.ps1"
 }
 
 function update_submodules() {
   info "Updating git submodules"
-  git submodule init
-  git submodule update --recursive --remote
+  git submodule --quiet update --init --recursive --remote --merge
 }
 
 # Package dedicated to the download of resources
